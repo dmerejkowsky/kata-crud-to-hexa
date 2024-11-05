a7f666dd300e3ab7c704083087ae5b891f9f9131
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-11-05 11:56:32 +0100

    Reformat java code

--- src/main/java/info/dmerej/train_reservation/TrainReservationApplication.java
+++ src/main/java/info/dmerej/train_reservation/TrainReservationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TrainReservationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrainReservationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrainReservationApplication.class, args);
+    }
 
 }
