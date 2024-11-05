2593854d19757041c49830bad957fd4d443abdb6
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-11-05 11:55:15 +0100

    Initial commit

---
+++ src/main/java/info/dmerej/train_reservation/TrainReservationApplication.java
@@ -0,0 +1,13 @@
+package info.dmerej.train_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/dmerej/train_reservation/TrainReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package info.dmerej.train_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
